1c690b8fab5aff13f4dea62fb9491f19e41bc2fe
Alexander <poltavetz.alexander@mail.ru>
2023-03-21 21:51:28 +0300

    Added display of movie pictures

--- app/src/main/java/com/example/movie/MainScreen.kt
+++ app/src/main/java/com/example/movie/MainScreen.kt
@@ -1,24 +1,33 @@
-package com.example.movie.ui.theme
+package com.example.movie
 
-import android.util.Log
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Card
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
-import com.example.movie.MainViewModel
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.annotation.ExperimentalCoilApi
+import coil.compose.rememberImagePainter
 import com.example.movie.data.models.Movies
-
 
 @Composable
 fun MainScreen(viewModel: MainViewModel) {
     viewModel.getAllMovies()
     val movies = viewModel.movies.observeAsState(listOf()).value
-    movies.forEach { Log.d("MainScreen", "Name: ${it.name}") }
-    Surface() {
-        LazyColumn {
+    Surface {
+        LazyColumn(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
             items(movies.take(10)) { item ->
                 MovieItem(item = item)
             }
@@ -26,9 +35,36 @@ fun MainScreen(viewModel: MainViewModel) {
     }
 }
 
+@OptIn(ExperimentalCoilApi::class)
 @Composable
 fun MovieItem(item: Movies) {
-    Row {
-        Text(text = item.name)
+    Card(
+        elevation = 4.dp,
+        modifier = Modifier
+            .padding(top = 8.dp)
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .wrapContentSize()
+                .padding(vertical = 16.dp)
+        ) {
+            Text(
+                text = item.name,
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Image(
+                painter = rememberImagePainter(item.image.medium),
+                contentDescription = "Film poster",
+                modifier = Modifier
+                    .size(256.dp)
+                    .padding(vertical = 3.dp)
+            )
+            Text(
+                text = "Rating: ${item.rating.average}",
+                fontSize = 24.sp
+            )
+        }
     }
 }
